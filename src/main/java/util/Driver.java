8ad11f1df8ff32a48ad7bd08f31802d4cf62a00e
Ajay Kalra <nsh.klr1@gmail.com>
2020-03-15 09:47:21 -0500

    headless mode

--- src/main/java/util/Driver.java
+++ src/main/java/util/Driver.java
@@ -1,16 +1,18 @@
 package util;
 
+import com.maxsoft.webdrivermanager.driver.DriverFactory;
 import com.thoughtworks.gauge.AfterScenario;
 import com.thoughtworks.gauge.BeforeScenario;
 import org.openqa.selenium.WebDriver;
 
 public class Driver {
 
+    private static final String BROWSER = System.getenv("browser"); // broser came from java property file from any property file  which ever browser we put in maven command
     public static WebDriver driver;
 
     @BeforeScenario
     public void startBrowser() {
-        driver = DriverFactory.getDriver();
+        driver = DriverFactory.getDriver(BROWSER);
         driver.manage().window().maximize();
         driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
     }
